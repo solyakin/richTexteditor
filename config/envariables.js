571ee589257cffa813a9856d049c76373e5fd889
solomon Akinlade <48894861+solyakin@users.noreply.github.com>
2023-03-23 07:12:43 +0100

    Update envariables.js

--- config/envariables.js
+++ config/envariables.js
@@ -1,3 +1,3 @@
-export const CLOUD_NAME= 'duhkz21x4'
-export const CLOUDINARY_PRESET= 'arh5cber'
-export const CLOUDINARY_CLIENTAPI= 'https://api.cloudinary.com/v1_1/duhkz21x4/image/upload'
+export const CLOUD_NAME= ''
+export const CLOUDINARY_PRESET= ''
+export const CLOUDINARY_CLIENTAPI= ''
